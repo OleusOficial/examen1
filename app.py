34cc1986cb8df411e3d0c2836d1ea523cb3d1750
OleusOficial <androidkimbiso@gmail.com>
2016-11-11 15:54:42 -0600

    Exames-Ubicacion de Tiendas

---
+++ app.py
@@ -0,0 +1,19 @@
+import web
+import json
+
+
+render = web.template.render('views/')
+urls = ('/index(.*)', 'index')
+
+class index:
+    def GET(self, data):
+        data = []
+        with open('data.json', 'r') as file:
+            temp_data = json.load(file)
+            data = temp_data['results']
+        return render.index(data)      
+         
+if __name__ == '__main__':
+    app = web.application(urls, globals())
+    web.config.debug = True
+    app.run()  
