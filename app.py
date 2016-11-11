c9a014b66c252e21f75b1f2b86dec88528802258
OleusOficial <androidkimbiso@gmail.com>
2016-11-11 16:09:46 -0600

    Examen#

--- app.py
+++ app.py
@@ -3,15 +3,11 @@ import json
 
 
 render = web.template.render('views/')
-urls = ('/(.*)', 'index')
+urls = ('/', 'index')
 
 class index:
-    def GET(self, data):
-        data = []
-        with open('data.json', 'r') as file:
-            temp_data = json.load(file)
-            data = temp_data['results']
-        return render.index(data)      
+    def GET(self ): 
+        return render.index( )      
          
 if __name__ == '__main__':
     app = web.application(urls, globals())
